853cb2354d313ea3d1f1cd1087a0eab781cb8c69
butthx <tanya.duwi23@gmail.com>
2021-09-11 12:40:47 +0700

    docs

--- src/index.ts
+++ src/index.ts
@@ -10,25 +10,4 @@ export { Snake } from './snake/client';
 export { Telegram } from './snake/Telegram';
 export * as Wizard from './snake/wizard';
 export * as Update from './snake/Update';
-// importing gramjs
-import { Api } from 'telegram/tl';
-import * as Tl from 'telegram/tl';
-import * as Helpers from 'telegram/Helpers';
-import * as Password from 'telegram/Password';
-import * as Utils from 'telegram/Utils';
-import * as EntityCache from 'telegram/entityCache';
-import * as Version from 'telegram/Version';
-import * as TelegramClient from 'telegram/client/TelegramClient';
-import * as Uploads from 'telegram/client/uploads';
-import * as MessageParse from 'telegram/client/messageParse';
-export let GramJs = {
-  Api,
-  Tl,
-  Helpers,
-  Password,
-  Utils,
-  Version,
-  TelegramClient,
-  Uploads,
-  MessageParse,
-};
+export * as GramJs from 'telegram';
