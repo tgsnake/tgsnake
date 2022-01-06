d04fbe022faa2e8112ae79d7beeb6ddc4e3e0be1
butthx <68604099+butthx@users.noreply.github.com>
2022-01-06 19:45:59 +0700

    updating version

--- src/client.ts
+++ src/client.ts
@@ -51,7 +51,7 @@ function makeApiId(length) {
 export class Snake extends MainContext {
   client!: TelegramClient;
   telegram!: Telegram;
-  version: string = '2.0.0-beta.7';
+  version: string = '2.0.0-beta.8';
   logger: string = 'none';
   options!: Options;
   constructor(options?: Options) {
