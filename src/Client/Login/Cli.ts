acb7f45670790443835d2e82d66d9ee454f94435
butthx <68604099+butthx@users.noreply.github.com>
2022-09-11 19:29:35 +0700

    Improve login via cli

---
+++ src/Client/Login/Cli.ts
@@ -0,0 +1,205 @@
+/**
+ * tgsnake - Telegram MTProto framework for nodejs.
+ * Copyright (C) 2022 butthx <https://github.com/butthx>
+ *
+ * THIS FILE IS PART OF TGSNAKE
+ *
+ * tgsnake is a free software : you can redistribute it and/or modify
+ * it under the terms of the MIT License as published.
+ */
+import type { Snake } from '../Snake';
+import type { Options } from '../Options';
+import { Logger } from '../../Context/Logger';
+import { Client, Raw } from '@tgsnake/core';
+import prompts from 'prompts';
+
+export async function LoginWithCLI(snake: Snake): Promise<Raw.users.UserFull> {
+  Logger.info('Initial login with CLI.');
+  if (!snake._options.apiId) {
+    Logger.debug('Api Id is missing.');
+    await AskApiId(snake);
+  }
+  if (!snake._options.apiHash) {
+    Logger.debug('Api Hash is missing.');
+    await AskApiHash(snake);
+  }
+  Logger.debug('Creating client.');
+  snake._client = new Client(
+    // @ts-ignore
+    snake._options.login.session,
+    snake._options.apiHash,
+    snake._options.apiId,
+    snake._options.clientOptions
+  );
+  // @ts-ignore
+  await snake._options.login.session.load();
+  // @ts-ignore
+  if (!snake._options.login.session?.authKey) {
+    if (snake._options.login.botToken) {
+      Logger.debug('Login using bot token.');
+      const user = await snake._client.start({
+        botToken: snake._options.login.botToken!,
+      });
+      Logger.log(
+        `${
+          snake._options.login.forceDotSession
+            ? `(${snake._options.login.sessionName}.session)`
+            : ``
+        } Loggined as: `
+      );
+      // @ts-ignore
+      await snake._options.login.session.save();
+      return user;
+    }
+    const loginAs = await AskLoginAs();
+    Logger.debug(`Login as: ${loginAs}`);
+    if (loginAs === 'bot') {
+      await AskBotToken(snake);
+      const user = await snake._client.start({
+        botToken: snake._options.login.botToken!,
+      });
+      Logger.log(
+        `${
+          snake._options.login.forceDotSession
+            ? `(${snake._options.login.sessionName}.session)`
+            : ``
+        } Loggined as: `
+      );
+      // @ts-ignore
+      await snake._options.login.session.save();
+      return user;
+    }
+    const user = await snake._client.start({
+      phoneNumber: AskPhoneNumber,
+      password: AskPassword,
+      recoveryCode: AskRecoveryCode,
+      code: AskOTPCode,
+      firstname: AskFirstName,
+      lastname: AskLastName,
+    });
+    Logger.log(
+      `${
+        snake._options.login.forceDotSession ? `(${snake._options.login.sessionName}.session)` : ``
+      } Loggined as: `
+    );
+    // @ts-ignore
+    await snake._options.login.session.save();
+    return user;
+  } else {
+    await snake._client.connect();
+    return await snake._client.getMe();
+  }
+}
+async function AskApiId(snake: Snake): Promise<string> {
+  const { value } = await prompts({
+    type: 'number',
+    name: 'value',
+    message: 'Input your api id!',
+    validate: (value) => (value ? true : false),
+  });
+  snake._options.apiId = value;
+  Logger.info(`Setting up Api Id with: ${snake._options.apiId}.`);
+  return String(value);
+}
+async function AskApiHash(snake: Snake): Promise<string> {
+  const { value } = await prompts({
+    type: 'text',
+    name: 'value',
+    message: 'Input your api hash!',
+    validate: (value) => (value ? true : false),
+  });
+  snake._options.apiHash = value;
+  Logger.info(`Setting up Api Hash with: ${snake._options.apiHash}.`);
+  return String(value);
+}
+async function AskLoginAs(): Promise<string> {
+  const { value } = await prompts({
+    type: 'select',
+    name: 'value',
+    message: 'Login as?',
+    initial: 0,
+    choices: [
+      {
+        title: 'Bot',
+        description: 'Login as bot using bot token from bot father.',
+        value: 'bot',
+      },
+      {
+        title: 'User',
+        description: 'Login as user using phone number.',
+        value: 'user',
+      },
+    ],
+    validate: (value) => (value ? true : false),
+  });
+  return String(value);
+}
+async function AskBotToken(snake: Snake): Promise<string> {
+  const { value } = await prompts({
+    type: 'text',
+    name: 'value',
+    message: 'Input your bot token!',
+    validate: (value) => (value ? true : false),
+  });
+  snake._options.login.botToken = value;
+  const splitedBotToken = String(snake._options.login.botToken).split(':');
+  const securedBotToken = `${splitedBotToken[0]}:${splitedBotToken[1]
+    .slice(30)
+    .padStart(splitedBotToken[1].length, '*')}`;
+  Logger.info(`Setting up Bot Token with: ${securedBotToken}.`);
+  return String(value);
+}
+async function AskPhoneNumber(): Promise<string> {
+  const { value } = await prompts({
+    type: 'number',
+    name: 'value',
+    message: 'Input your international phone number!',
+    validate: (value) => (value ? true : false),
+  });
+  return String(value);
+}
+async function AskPassword(hint: string): Promise<string> {
+  const { value } = await prompts({
+    type: 'password',
+    name: 'value',
+    message: `Input your two factor authentication password!\n\nHint : ${hint}`,
+    validate: (value) => (value ? true : false),
+  });
+  return String(value);
+}
+async function AskRecoveryCode(): Promise<string> {
+  const { value } = await prompts({
+    type: 'number',
+    name: 'value',
+    message: 'Input your recovery code from your email address!',
+    validate: (value) => (value ? true : false),
+  });
+  return String(value);
+}
+async function AskOTPCode(): Promise<string> {
+  const { value } = await prompts({
+    type: 'number',
+    name: 'value',
+    message: 'Input your otp code from telegram application or sms!',
+    validate: (value) => (value ? true : false),
+  });
+  return String(value);
+}
+async function AskFirstName(): Promise<string> {
+  const { value } = await prompts({
+    type: 'text',
+    name: 'value',
+    message: 'Input your first name!',
+    validate: (value) => (value ? true : false),
+  });
+  return String(value);
+}
+async function AskLastName(): Promise<string> {
+  const { value } = await prompts({
+    type: 'text',
+    name: 'value',
+    message: 'Input your last name!',
+    validate: (value) => (value ? true : false),
+  });
+  return String(value);
+}
