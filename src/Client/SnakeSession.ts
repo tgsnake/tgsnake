632248e5d50ca1b3e89122b5a32e04ee161508cf
butthx <68604099+butthx@users.noreply.github.com>
2022-09-11 08:05:29 +0700

    Introduction dot session

--- src/Client/SnakeSession.ts
+++ src/Client/SnakeSession.ts
@@ -1,213 +1,196 @@
-// Tgsnake - Telegram MTProto framework developed based on gram.js.
-// Copyright (C) 2022 Butthx <https://github.com/butthx>
-//
-// This file is part of Tgsnake
-//
-// Tgsnake is a free software : you can redistribute it and/or modify
-//  it under the terms of the MIT License as published.
+/**
+ * tgsnake - Telegram MTProto framework for nodejs.
+ * Copyright (C) 2022 butthx <https://github.com/butthx>
+ *
+ * THIS FILE IS PART OF TGSNAKE
+ *
+ * tgsnake is a free software : you can redistribute it and/or modify
+ * it under the terms of the MIT License as published.
+ */
+import { Storages, Raws } from '@tgsnake/core';
+import * as fs from 'fs';
+import * as path from 'path';
+import { Logger } from '../Context/Logger';
 
-import { MemorySession } from 'telegram/sessions/Memory';
-import { AuthKey } from 'telegram/crypto/AuthKey';
-import bigInt from 'big-integer';
-import { ResultGetEntity } from '../Telegram/Users/GetEntity';
-import fs from 'fs';
-import { Snake } from './Snake';
-
-export interface InterfaceSnakeSession {
-  authKey: any;
-  dcId: number;
-  port: number;
-  serverAddress: string;
-  entities: Array<any>;
-}
-let ignore = ['session.json', 'cache.json'];
-const CURRENT_VERSION = '1';
-export class SnakeSession extends MemorySession {
-  /** @hidden */
-  private _sessionName!: string;
-  /** @hidden */
-  private client!: Snake;
-  constructor(sessionName: string, client: Snake) {
+export class SnakeSession extends Storages.BaseSession {
+  private _name!: string;
+  constructor(name: string) {
     super();
-    this._sessionName = sessionName;
-    this.client = client;
+    this._name = name;
   }
   async load() {
-    if (fs.existsSync(`${process.cwd()}/${this._sessionName}`)) {
-      let dir = fs.readdirSync(`${process.cwd()}/${this._sessionName}`);
-      if (dir.includes('session.json')) {
-        let json: any = undefined;
-        try {
-          json = JSON.parse(
-            fs.readFileSync(`${process.cwd()}/${this._sessionName}/session.json`, 'utf8')
-          );
-        } catch (error) {
-          // if can't parse the session file, delete it.
-          fs.unlinkSync(`${process.cwd()}/${this._sessionName}/session.json`);
-          return;
-        }
-        if (!json) {
-          fs.unlinkSync(`${process.cwd()}/${this._sessionName}/session.json`);
-          return;
-        }
-        let authKey = json.authKey;
-        if (authKey && typeof authKey === 'object') {
-          this._authKey = new AuthKey();
-          if ('data' in authKey) {
-            authKey = Buffer.from(authKey.data);
-          }
-          await this._authKey.setKey(authKey);
-        }
-        if (json?.dcId) this._dcId = json?.dcId;
-        if (json?.port) this._port = json?.port;
-        if (json?.serverAddress) this._serverAddress = json?.serverAddress;
-      } else {
-        // importing from storeSession then remove it.
-        if (dir.includes(`${this._sessionName}%3AauthKey`)) {
-          let authKey = JSON.parse(
-            fs.readFileSync(
-              `${process.cwd()}/${this._sessionName}/${this._sessionName}%3AauthKey`,
-              'utf8'
-            )
-          );
-          if (authKey && typeof authKey === 'object') {
-            this._authKey = new AuthKey();
-            if ('data' in authKey) {
-              authKey = Buffer.from(authKey.data);
-            }
-            await this._authKey.setKey(authKey);
-          }
-        }
-        if (dir.includes(`${this._sessionName}%3AdcId`)) {
-          let dcId = fs.readFileSync(
-            `${process.cwd()}/${this._sessionName}/${this._sessionName}%3AdcId`,
-            'utf8'
-          );
-          if (!isNaN(Number(dcId))) this._dcId = Number(dcId);
-        }
-        if (dir.includes(`${this._sessionName}%3Aport`)) {
-          let port = fs.readFileSync(
-            `${process.cwd()}/${this._sessionName}/${this._sessionName}%3Aport`,
-            'utf8'
-          );
-          if (!isNaN(Number(port))) this._port = Number(port);
-        }
-        if (dir.includes(`${this._sessionName}%3AserverAddress`)) {
-          let serverAddress = fs.readFileSync(
-            `${process.cwd()}/${this._sessionName}/${this._sessionName}%3AserverAddress`,
-            'utf8'
-          );
-          if (serverAddress) this._serverAddress = String(serverAddress).replace(/\"|\'/g, '');
-        }
-        fs.writeFileSync(
-          `${process.cwd()}/${this._sessionName}/session.json`,
-          JSON.stringify({
-            authKey: this._authKey?.getKey(),
-            dcId: this._dcId,
-            port: this._port,
-            serverAddress: this._serverAddress,
-          })
-        );
-        for (let file of dir) {
-          if (!ignore.includes(file)) {
-            fs.unlinkSync(`${process.cwd()}/${this._sessionName}/${file}`);
-          }
-        }
-      }
+    let sessionName = `${this._name}.session`;
+    if (fs.existsSync(path.join(process.cwd(), sessionName))) {
+      let start = Math.floor(Date.now() / 1000);
+      let bytes = fs.readFileSync(path.join(process.cwd(), sessionName));
+      Logger.debug(`Session have a ${bytes.length} bytes`);
+      this._dcId = bytes.readUInt8(0); // 1
+      Logger.debug(`Found dcId: ${this._dcId}.`);
+      this._apiId = bytes.readUInt32LE(1); // 5
+      Logger.debug(`Found apiId: ${this._apiId}.`);
+      this._testMode = bytes.readUInt8(5) ? true : false; // 6
+      Logger.debug(`Found testMode: ${this._testMode}.`);
+      this._authKey = bytes.slice(6, 262); // 262
+      Logger.debug(`Found authKey: ${this._authKey.length} bytes.`);
+      this._userId = BigInt(`0x${bytes.slice(262, 270).toString('hex')}`); // 270
+      Logger.debug(`Found userId: ${this._userId}.`);
+      this._isBot = bytes.readUInt8(270) ? true : false; // 271
+      Logger.debug(`Found isBot: ${this._isBot}.`);
+      Logger.debug(`Done parsing string session (${Math.floor(Date.now() / 1000) - start}s)`);
+    }
+    for (let peer of await this._loadCache()) {
+      this._peers.set(peer[0], peer);
     }
   }
-  setDC(dcId: number, serverAddress: string, port: number) {
-    super.setDC(dcId, serverAddress, port);
-    const create = () => {
-      let dir = fs.readdirSync(`${process.cwd()}/${this._sessionName}`);
+  async save() {
+    let sessionName = `${this._name}.session`;
+    let cacheName = `${this._name}.cache`;
+    // save session when it unavailable.
+    if (!path.join(process.cwd(), sessionName)) {
       fs.writeFileSync(
-        `${process.cwd()}/${this._sessionName}/session.json`,
-        JSON.stringify({
-          authKey: this._authKey?.getKey(),
-          dcId: dcId,
-          port: port,
-          serverAddress: serverAddress,
-        })
+        path.join(process.cwd(), sessionName),
+        Buffer.from(await this.exportString(), 'base64url')
       );
-      for (let file of dir) {
-        if (!ignore.includes(file)) {
-          fs.unlinkSync(`${process.cwd()}/${this._sessionName}/${file}`);
+      Logger.info(`Session saved to: "${path.join(process.cwd(), sessionName)}".`);
+    }
+    fs.writeFileSync(path.join(process.cwd(), cacheName), await this._makeCache());
+    Logger.info(`Cache saved to: "${path.join(process.cwd(), cacheName)}".`);
+  }
+  /**
+   * Load the tgsnake peers cache.
+   */
+  private async _loadCache(): Promise<
+    Array<[id: bigint, accessHash: bigint, type: string, username?: string, phoneNumber?: string]>
+  > {
+    let peer: Array<any> = [];
+    let cacheName = `${this._name}.cache`;
+    if (fs.existsSync(path.join(process.cwd(), cacheName))) {
+      let bytes = new Raws.BytesIO(fs.readFileSync(path.join(process.cwd(), cacheName)));
+      let length = Raws.Primitive.Int.read(bytes);
+      // bytes[VectorLength + VectorBytes[bytes[contentLength + content]]]
+      for (let i = 0; i < length; i++) {
+        let count = Raws.Primitive.Int.read(bytes);
+        peer.push(buildPeerFromBytes(bytes.read(count)));
+      }
+    }
+    return peer as unknown as Array<
+      [id: bigint, accessHash: bigint, type: string, username?: string, phoneNumber?: string]
+    >;
+  }
+  /**
+   * Make a bytes cache of peers.
+   */
+  private async _makeCache(): Promise<Buffer> {
+    let count = 0;
+    let bytes = new Raws.BytesIO();
+    for (let [key, value] of this._peers) {
+      count += 1;
+      let content = await buildBytesFromPeer(value);
+      bytes.write(Buffer.concat([Raws.Primitive.Int.write(content.length), content]));
+    }
+    return Buffer.concat([Raws.Primitive.Int.write(count), bytes.buffer]);
+  }
+  /** @hidden */
+  [Symbol.for('nodejs.util.inspect.custom')](): { [key: string]: any } {
+    const toPrint: { [key: string]: any } = {
+      _: this.constructor.name,
+    };
+    for (const key in this) {
+      if (this.hasOwnProperty(key)) {
+        const value = this[key];
+        if (!key.startsWith('_')) {
+          toPrint[key] = value;
         }
       }
-    };
-    if (fs.existsSync(`${process.cwd()}/${this._sessionName}`)) {
-      create();
-    } else {
-      fs.mkdirSync(`${process.cwd()}/${this._sessionName}`, {
-        recursive: true,
-      });
-      create();
     }
+    return toPrint;
   }
-  get authKey() {
-    return this._authKey;
-  }
-  set authKey(value: AuthKey | undefined) {
-    this._authKey = value;
-    const create = () => {
-      let dir = fs.readdirSync(`${process.cwd()}/${this._sessionName}`);
-      fs.writeFileSync(
-        `${process.cwd()}/${this._sessionName}/session.json`,
-        JSON.stringify({
-          authKey: value?.getKey(),
-          dcId: this._dcId,
-          port: this._port,
-          serverAddress: this._serverAddress,
-        })
-      );
-      for (let file of dir) {
-        if (!ignore.includes(file)) {
-          fs.unlinkSync(`${process.cwd()}/${this._sessionName}/${file}`);
+  /**@hidden*/
+  toJSON(): { [key: string]: any } {
+    const toPrint: { [key: string]: any } = {
+      _: this.constructor.name,
+    };
+    for (const key in this) {
+      if (this.hasOwnProperty(key)) {
+        const value = this[key];
+        if (!key.startsWith('_')) {
+          toPrint[key] = typeof value === 'bigint' ? String(value) : value;
         }
       }
-    };
-    if (fs.existsSync(`${process.cwd()}/${this._sessionName}`)) {
-      create();
-    } else {
-      fs.mkdirSync(`${process.cwd()}/${this._sessionName}`, {
-        recursive: true,
-      });
-      create();
     }
+    return toPrint;
   }
-  getEntityRowsById(id: string | bigInt.BigInteger, exact: boolean = true) {
-    let Id: string | bigint = bigInt.isInstance(id) ? BigInt(String(id)) : String(id);
-    // get from cache
-    let cache = this.client.entityCache.get(Id);
-    if (!cache) return [];
-    return [
-      String(cache.id),
-      String(cache.accessHash),
-      cache.username ? cache.username : '',
-      cache.phone ? cache.phone : '',
-      cache.lastName ? cache.firstName + ' ' + cache.lastName : cache.firstName,
-      new Date().getTime().toString(),
-    ];
+  /** @hidden */
+  toString(): string {
+    return `[constructor of ${this.constructor.name}] ${JSON.stringify(this, null, 2)}`;
   }
-  save() {
-    if (!this.authKey || !this.serverAddress || !this.port) {
-      return '';
+}
+/**
+ * Creating valid bytes from peer schema.
+ * @param peer {Array} - Peer will be convert to bytes
+ */
+export function buildBytesFromPeer(
+  peer: [id: bigint, accessHash: bigint, type: string, username?: string, phoneNumber?: string]
+): Buffer {
+  let bytes = new Raws.BytesIO();
+  let flags = 0;
+  if (peer[3]) {
+    flags |= 1 << 4;
+  }
+  if (peer[4]) {
+    flags |= 1 << 5;
+  }
+  bytes.write(Raws.Primitive.Int.write(flags));
+  bytes.write(Raws.Primitive.Long.write(peer[0]));
+  bytes.write(Raws.Primitive.Long.write(peer[1]));
+  bytes.write(Raws.Primitive.String.write(peer[2]));
+  if (peer[3]) {
+    bytes.write(Raws.Primitive.String.write(peer[3]));
+  }
+  if (peer[4]) {
+    bytes.write(Raws.Primitive.String.write(peer[4]));
+  }
+  return bytes.buffer;
+}
+/**
+ * Creating valid peer schema from bytes.
+ * @param bytes {Buffer} - Bytes will be converted to peer schema.
+ */
+export function buildPeerFromBytes(
+  bytes: Buffer
+): [id: bigint, accessHash: bigint, type: string, username?: string, phoneNumber?: string] {
+  let b = new Raws.BytesIO(bytes);
+  // @ts-ignore
+  let results: Array<any> = [];
+  let flags = Raws.Primitive.Int.read(b);
+  results.push(Raws.Primitive.Long.read(b));
+  results.push(Raws.Primitive.Long.read(b));
+  results.push(Raws.Primitive.String.read(b));
+  if (flags & (1 << 4)) {
+    results.push(Raws.Primitive.String.read(b));
+  }
+  if (flags & (1 << 5)) {
+    results.push(Raws.Primitive.String.read(b));
+  }
+  return results as unknown as [
+    id: bigint,
+    accessHash: bigint,
+    type: string,
+    username?: string,
+    phoneNumber?: string
+  ];
+}
+
+export function generateName(base: string): string {
+  let i = 0;
+  while (true) {
+    let name = i === 0 ? base : `${base}${i}`;
+    if (
+      !fs.existsSync(path.join(process.cwd(), `${name}.session`)) &&
+      !fs.existsSync(path.join(process.cwd(), `${name}.cache`))
+    ) {
+      return name;
     }
-    const key = this.authKey.getKey();
-    if (!key) {
-      return '';
-    }
-    const dcBuffer = Buffer.from([this.dcId]);
-    const addressBuffer = Buffer.from(this.serverAddress);
-    const addressLengthBuffer = Buffer.alloc(2);
-    addressLengthBuffer.writeInt16BE(addressBuffer.length, 0);
-    const portBuffer = Buffer.alloc(2);
-    portBuffer.writeInt16BE(this.port, 0);
-    return (
-      CURRENT_VERSION +
-      Buffer.concat([dcBuffer, addressLengthBuffer, addressBuffer, portBuffer, key]).toString(
-        'base64'
-      )
-    );
+    i++;
   }
 }
