f9127cbb99f3c4d780c8b72d54ead19adb92b588
butthx <68604099+butthx@users.noreply.github.com>
2023-03-22 11:15:08 +0700

    add: possible plugin to make their json update

--- src/Context/MainContext.ts
+++ src/Context/MainContext.ts
@@ -28,13 +28,19 @@ export class MainContext<T = {}> extends Composer<T> {
         `bot.use is unavailable when bot running. so kill bot first then add bot.use in your source code then running again.`
       );
     };
+    const parsed = await this.parseUpdate(update, client);
     try {
       // @ts-ignore
-      await run<Raw.TypeUpdate>(this.middleware(), update);
+      await run<Raw.TypeUpdate>(this.middleware(), parsed);
     } catch (error: any) {
       // @ts-ignore
-      return this._errorHandler(error, update);
+      return this._errorHandler(error, parsed);
     }
+  }
+  async parseUpdate(update: Raw.TypeUpdate, client: Snake): Promise<object> {
+    // Why Promise<object> ? because the return of parseUpdate is can by anything, but it must be a class or json object.
+    // Possible plugin for make their own parse function.
+    return update;
   }
   catch(errorHandler: ErrorHandler<T>) {
     if (typeof errorHandler === 'function') {
